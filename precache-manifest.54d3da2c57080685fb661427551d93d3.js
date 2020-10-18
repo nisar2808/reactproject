3cac9563b940bbdbe7f26e6d0b44cbcdf4db8662
NISAR <itintern@earnwealth.in>
2020-10-18 15:43:36 +0530

    Updates

---
+++ precache-manifest.54d3da2c57080685fb661427551d93d3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa0a30718f020348ed26b783615e024",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "431b683904236106ad06",
+    "url": "/reactwebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "ff0ad5a82c4203c5c6d2",
+    "url": "/reactwebsite/static/css/main.eef36b30.chunk.css"
+  },
+  {
+    "revision": "431b683904236106ad06",
+    "url": "/reactwebsite/static/js/2.3e2b6642.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactwebsite/static/js/2.3e2b6642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0ad5a82c4203c5c6d2",
+    "url": "/reactwebsite/static/js/main.5d95c0b2.chunk.js"
+  },
+  {
+    "revision": "b2418759a829b98ef995",
+    "url": "/reactwebsite/static/js/runtime-main.9b48dea8.js"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/reactwebsite/static/media/aboutus.619f0cc8.png"
+  },
+  {
+    "revision": "121091c6ffc08569205852bb237fdd14",
+    "url": "/reactwebsite/static/media/android.121091c6.jpg"
+  },
+  {
+    "revision": "60cf9e5c66b10ba65230ae78ff114ef6",
+    "url": "/reactwebsite/static/media/app.60cf9e5c.jpg"
+  },
+  {
+    "revision": "f8011340caf6112d8998a65f47bbc026",
+    "url": "/reactwebsite/static/media/digital.f8011340.png"
+  },
+  {
+    "revision": "806d626c4b1bb01172b4eadf1eb6e9b3",
+    "url": "/reactwebsite/static/media/marketing.806d626c.png"
+  },
+  {
+    "revision": "e981619f086d13be896e36768c26ce8f",
+    "url": "/reactwebsite/static/media/product-launch-rocket-man.e981619f.svg"
+  },
+  {
+    "revision": "0996d71ad36a0607f0641b5e9156f628",
+    "url": "/reactwebsite/static/media/software.0996d71a.png"
+  },
+  {
+    "revision": "512999ef541e05c4c2163d9c69351459",
+    "url": "/reactwebsite/static/media/web.512999ef.jpg"
+  }
+]);
